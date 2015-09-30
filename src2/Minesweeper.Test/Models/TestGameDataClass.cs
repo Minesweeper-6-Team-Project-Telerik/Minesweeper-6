2df55b3f6b1519b169fcebfdb3b6fadfe812a38b
TanyaRan <tatyanaran@gmail.com>
2015-09-30 20:43:05 +0300

    Create TestGameDataClass.cs
    How to test Load and Save

---
+++ src2/Minesweeper.Test/Models/TestGameDataClass.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Minesweeper.Test.Models
+{
+    using Minesweeper.Models;
+    using Minesweeper.Models.Exceptions;
+
+    [TestClass]
+    class TestGameDataClass
+    {
+        [TestMethod]
+        public void TestSaveSerializableObject()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestLoadSerializableObject()
+        {
+
+        }
+    }
+}
